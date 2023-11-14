43adba388b621aced3775324cfb86878e5ee33db
KristiBu <106319724+KristiBu888@users.noreply.github.com>
2023-11-14 23:56:53 +0300

    hw ready

---
+++ task_1.py
@@ -0,0 +1,25 @@
+# Задача 32: Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не
+# меньше заданного минимума и не больше заданного
+# максимума)
+# Ввод: [-5, 9, 0, 3, -1, -2, 1,
+# 4, -2, 10, 2, 0, -9, 8, 10, -9,
+# 0, -5, -5, 7]
+# Вывод: [1, 9, 13, 14, 19]
+
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = 0
+max_number = 10
+
+
+def index_function(list_1, min_number, max_number):
+    list_index = []
+    for i in range(len(list_1)):
+        if min_number <= list_1[i] <= max_number:
+            list_index.append(i)
+    return list_index
+
+
+result = index_function(list_1, min_number, max_number)
+print(*result, sep='\n')
